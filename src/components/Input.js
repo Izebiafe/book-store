30937a9ef050201a98c074d2b54239c8221ef287
izebiafe <afimiafidelis95@gmail.com>
2023-09-28 11:39:24 +0100

    Restructure and style input form

--- src/components/Input.js
+++ src/components/Input.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { v4 as uuidv4 } from 'uuid';
 import { addBook } from '../redux/books/booksSlice';
+import './styles/input.css';
 
 const Input = () => {
   const [title, setTitle] = useState('');
@@ -9,12 +10,10 @@ const Input = () => {
   const [category, setCategory] = useState('');
   const [errorMessage, setErrorMessage] = useState('');
   const dispatch = useDispatch();
-
   const submitHandler = (e) => {
     e.preventDefault();
     if (!title || !author || !category) {
       setErrorMessage('Please fill in all the fields before adding the book.');
-
       return;
     }
     const newBook = {
@@ -28,19 +27,42 @@ const Input = () => {
     setAuthor('');
     setCategory('');
   };
-
   return (
     <div className="form-container">
       <h2>Add New Book</h2>
-      <form>
-        <input type="text" placeholder="Book title" id="title" value={title} onChange={(e) => setTitle(e.target.value)} />
-        <input type="text" placeholder="Book author" id="author" value={author} onChange={(e) => setAuthor(e.target.value)} />
-        <input type="text" placeholder="Book category" id="category" value={category} onChange={(e) => setCategory(e.target.value)} />
+      <form className="input-form">
+        <input
+          type="text"
+          placeholder="Book title"
+          id="title"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          className="title"
+        />
+        <input
+          type="text"
+          placeholder="Book author"
+          id="author"
+          value={author}
+          onChange={(e) => setAuthor(e.target.value)}
+          className="author"
+        />
+        <div className="category-input">
+          <input
+            type="text"
+            placeholder="Category"
+            id="category"
+            value={category}
+            onChange={(e) => setCategory(e.target.value)}
+            className="category"
+          />
+          <i className="fas fa-caret-down" />
+        </div>
+
         {errorMessage && <div className="text-danger error-message">{errorMessage}</div>}
         <button type="submit" id="add-new-book" onClick={submitHandler}>ADD BOOK</button>
       </form>
     </div>
   );
 };
-
 export default Input;
