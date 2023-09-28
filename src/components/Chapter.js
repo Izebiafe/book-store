f8f8eb558d6414045d1f16f3f5009d8c193f2ce5
izebiafe <afimiafidelis95@gmail.com>
2023-09-28 08:56:19 +0100

    add chapter component

---
+++ src/components/Chapter.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Chapter = () => (
+  <div>
+    <p>CURRENT CHAPTER</p>
+    <p>Chapter 17</p>
+    <button type="button">UPDATE PROGRESS</button>
+  </div>
+);
+
+export default Chapter;
