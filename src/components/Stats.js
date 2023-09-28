a5f311146d00556fcc98026fed7ded424cd59353
izebiafe <afimiafidelis95@gmail.com>
2023-09-28 08:58:24 +0100

    add book stat component

---
+++ src/components/Stats.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Stats = () => (
+  <div>
+    <div className="oval" />
+    <div className="stats">
+      <h2>35%</h2>
+      <p>completed</p>
+    </div>
+  </div>
+);
+
+export default Stats;
